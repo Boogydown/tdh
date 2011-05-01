aa66989c8301cef950c59ed901524f33225f63a5
Nathaniel Eliot <temujin9@t9productions.com>
2011-05-01 11:59:23 -0500

    Moving application code to a separate file

---
+++ _attachments/script/app.js
@@ -0,0 +1,6 @@
+$.couch.app(function(app) {
+    $("#account").evently("account", app);
+    $("#profile").evently("profile", app);
+    $.evently.connect("#account","#profile", ["loggedIn","loggedOut"]);
+    $("#items").evently("items", app);
+});

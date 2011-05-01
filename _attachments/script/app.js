b7e8b80fa145ec4575ddb3028a5546558f15d4c4
Nathaniel Eliot <temujin9@t9productions.com>
2011-05-01 12:23:09 -0500

    Trying out the default backbone-couchdb application

--- _attachments/script/app.js
+++
@@ -1,6 +0,0 @@
-$.couch.app(function(app) {
-    $("#account").evently("account", app);
-    $("#profile").evently("profile", app);
-    $.evently.connect("#account","#profile", ["loggedIn","loggedOut"]);
-    $("#items").evently("items", app);
-});

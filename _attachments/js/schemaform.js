6bb983181fe6cb4a3d70dc94adc6c2eaa4249526
Nathaniel Eliot <temujin9@t9productions.com>
2011-05-04 12:52:30 -0500

    Refactoring FormView

--- _attachments/js/schemaform.js
+++ _attachments/js/schemaform.js
@@ -1,18 +1,5 @@
-$(function(){
-    var Schema = {};
+var Schema.Form = function () {
+    alert("this is a test");
+};
 
-    var Schema.Form = function () {
-        alert("this is a test");
-    };
-
-    if (typeof exports !== "undefined") {
-        module.exports.SchemaForm = Schema.Form;
-        module.exports.SchemaTable = Schema.Table;
-        module.exports.SchemaApp = Schema.App;
-    }
-    else {
-        SchemaForm = Schema.Form;
-//         SchemaTable = Schema.Table;
-//         SchemaApp = Schema.App;
-    }
-});
+SchemaForm = Schema.Form;

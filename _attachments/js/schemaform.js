73351ae66d0ee2bc094522ddf22eec3dfd4bf8de
Nathaniel Eliot <temujin9@t9productions.com>
2011-05-04 22:37:59 -0500

    Building SchemaFormView

--- _attachments/js/schemaform.js
+++ _attachments/js/schemaform.js
@@ -16,25 +16,26 @@ var SchemaForm = Backbone.View.extend({
         // Get the inputEx field definition from the "Comment" object 
         var inputExDefinition = schemaBuilder.schemaToInputEx(this.options.schema);
 
-        // Add 'container1' as parent element 
+        // Add 'model_edit' as parent element 
         inputExDefinition.parentEl = 'model_edit';
 
         // Create the form 
-        var f = inputEx(inputExDefinition);
+        this.form = inputEx(inputExDefinition);
         return this;
     },
 
     // Simply takes the vals from the input fields and 
     // creates a new Comment.
     onSubmit : function(){
-        var name = $("#name").val();
-        var text = $("#text").val();
-        // sanitize user input...you never know ;)
-        name = name.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
-        text = text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
-        Comments.create({
-            "name" : name,
-            "text" : text
-        });
+//         var name = $("#name").val();
+//         var text = $("#text").val();
+//         // sanitize user input...you never know ;)
+//         name = name.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
+//         text = text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
+//         Comments.create({
+//             "name" : name,
+//             "text" : text
+//         });
+        
     }
 });

b883b061285c427aa4f2d1d0291973f1f11b2aed
Nathaniel Eliot <temujin9@t9productions.com>
2011-05-04 22:04:08 -0500

    Building SchemaFormView

--- vendor/couchapp/_attachments/loader.js
+++ vendor/couchapp/_attachments/loader.js
@@ -14,7 +14,7 @@ couchapp_load([
   "js/lib/backbone-couchdb.js",
   "js/lib/dustjs/dist/dust-full-0.3.0.js",
   "js/lib/inputex/lib/yui/yuiloader/yuiloader.js",
-//   "js/lib/inputex/js/inputex-loader.js",
+  "js/lib/inputex/lib/yui/dom/dom.js",
   "js/lib/inputex/build/inputex.js",
   "js/schemaform.js",
   "js/app.js"

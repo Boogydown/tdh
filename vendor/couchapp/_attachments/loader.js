ff03789473b4c9e848afb217538724e8b1c7e19b
Nathaniel Eliot <temujin9@t9productions.com>
2011-05-04 20:26:07 -0500

    Refactoring FormView

--- vendor/couchapp/_attachments/loader.js
+++ vendor/couchapp/_attachments/loader.js
@@ -13,6 +13,8 @@ couchapp_load([
   "js/lib/backbone.js",
   "js/lib/backbone-couchdb.js",
   "js/lib/dustjs/dist/dust-full-0.3.0.js",
+  "http://yui.yahooapis.com/2.8.2r1/build/yuiloader/yuiloader-min.js",
+  "js/lib/inputex/js/inputex-loader.js",
   "js/schemaform.js",
   "js/app.js"
 ]);

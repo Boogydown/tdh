5c214ed1d0be9772b789ad8d032b40e2a588f6e5
Nathaniel Eliot <temujin9@t9productions.com>
2011-05-10 13:37:15 -0500

    Refactoring SchemaForm now that it submits correctly

--- _attachments/js/app.js
+++ _attachments/js/app.js
@@ -26,7 +26,7 @@ $(function(){
 	});
 	
 	// Now let's define a new Collection of Comments
-	var CommentList = Backbone.Collection.extend({
+	var CommentCollection = Backbone.Collection.extend({
 		// The couchdb-connector is capable of mapping the url scheme
 		// proposed by the authors of Backbone to documents in your database,
 		// so that you don't have to change existing apps when you switch the sync-strategy
@@ -37,8 +37,6 @@ $(function(){
 			return comment.get("date");
 		}
 	});
-	
-	var Comments = new CommentList();
 
         var DustView = Backbone.View.extend({
             registerTemplate : function(name) {
@@ -60,76 +58,6 @@ $(function(){
                 $(this.el).html(result);
                 return this;
             }
-        });
-
-        // This view is responsible for creating the add/edit fields
-        var FormView = DustView.extend({
-                initialize : function(){
-                        // Add each field to the form in turn
-                        _.each(this.options.fields, function(field) {
-                            var foo = new FormFieldView(field);
-                            this.el.append(foo.render().el);
-                        }, this);
-                }
-        });
-
-        var FormFieldView = DustView.extend({
-                tagName : "p",
-
-                initialize : function(){
-                        this.options.type = this.options.type || "text";
-                        this.registerTemplate("input-" + this.options.type);
-                },
-                
-                getData : function(){
-                    return this.options;
-                }
-        });
-        
-        var CommentEditView = FormView.extend({
-                el : $("#model_edit"),
-                
-                events : {
-                        "click #send" : "onSubmit"
-                },
-                
-                initialize : function(){
-                        _.bindAll(this, "onSubmit");
-        
-                        this.options.fields = [
-                            {
-                                field_id: "name",
-                                description: "Name",
-                                type: "text"
-                            },
-                            {
-                                field_id: "text",
-                                description: "Text",
-                                placeholder: "Your text",
-                                type: "textarea"
-                            },
-                            {
-                                field_id: "send",
-                                type: "submit"
-                            }
-                        ];
-
-                        FormView.prototype.initialize.call(this);
-                },
-                
-                // Simply takes the vals from the input fields and 
-                // creates a new Comment.
-                onSubmit : function(){
-                        var name = $("#name").val();
-                        var text = $("#text").val();
-                        // sanitize user input...you never know ;)
-                        name = name.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
-                        text = text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
-                        Comments.create({
-                                "name" : name,
-                                "text" : text
-                        });
-                }
         });
         
 	// Represents an comment entry
@@ -247,9 +175,6 @@ $(function(){
             onSubmit : function(){
                 var name = $("#model_edit [name='name']").val();
                 var text = $("#model_edit [name='text']").val();
-                // sanitize user input...you never know ;)
-                name = name.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
-                text = text.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
                 Comments.create({
                     "name" : name,
                     "text" : text
@@ -259,28 +184,23 @@ $(function(){
         });
 
         
-        useSchemaForm = true;
-        if (useSchemaForm) {
-            comment_schema = {
-                "description":"A comment",
-                "type":"object",
-                "properties":{
-                    "name":{
-                        "description":"Name",
-                        "type":"string"
-                    },
-                    "text":{
-                        "description":"Comment",
-                        "placeholder": "Your text",
-                        "type":"text"
-                    }
+        var Comments = new CommentCollection();
+        comment_schema = {
+            "description":"A comment",
+            "type":"object",
+            "properties":{
+                "name":{
+                    "description":"Name",
+                    "type":"string"
+                },
+                "text":{
+                    "description":"Comment",
+                    "placeholder": "Your text",
+                    "type":"text"
                 }
-            };
-
-            new SchemaForm({ schema : comment_schema });
-        }
-        else new CommentEditView();
-
+            }
+        };
+        new SchemaForm({ schema : comment_schema });
 	new CommentsTable();
 	new App();
 

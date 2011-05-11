3e6ff4262c6a11d8555a0e0e232f4a160fec3bd7
Nathaniel Eliot <temujin9@t9productions.com>
2011-05-10 22:53:09 -0500

    Building SchemaTable

--- _attachments/js/app.js
+++ _attachments/js/app.js
@@ -1,42 +1,42 @@
 $(function(){
-	// Fill this with your database information.
-	// `ddocName` is the name of your couchapp project.
-	Backbone.couchConnector.databaseName = "schema_form";
-	Backbone.couchConnector.ddocName = "schema_form";
-	Backbone.couchConnector.viewName = "byCollection";
-	// If set to true, the connector will listen to the changes feed
-	// and will provide your models with real time remote updates.
-	Backbone.couchConnector.enableChanges = false;
-	
-	
-	// The model for a comment is kinda simple.
-	// We only need a name, a text and a date.
-	var CommentModel = Backbone.Model.extend({
-		initialize : function(){
-			if(!this.get("name")){
-				this.set({"name": "Anonymous"});
-			}
-			if(!this.get("text")){
-				this.set({"text": "Nothing"});
-			}
-			if(!this.get("date")){
-				this.set({"date": new Date().getTime()});
-			}
-		}
-	});
-	
-	// Now let's define a new Collection of Comments
-	var CommentCollection = Backbone.Collection.extend({
-		// The couchdb-connector is capable of mapping the url scheme
-		// proposed by the authors of Backbone to documents in your database,
-		// so that you don't have to change existing apps when you switch the sync-strategy
-		url : "/comments",
-		model : CommentModel,
-		// The comments should be ordered by date
-		comparator : function(comment){
-			return comment.get("date");
-		}
-	});
+        // Fill this with your database information.
+        // `ddocName` is the name of your couchapp project.
+        Backbone.couchConnector.databaseName = "schema_form";
+        Backbone.couchConnector.ddocName = "schema_form";
+        Backbone.couchConnector.viewName = "byCollection";
+        // If set to true, the connector will listen to the changes feed
+        // and will provide your models with real time remote updates.
+        Backbone.couchConnector.enableChanges = false;
+        
+        
+        // The model for a comment is kinda simple.
+        // We only need a name, a text and a date.
+        var CommentModel = Backbone.Model.extend({
+                initialize : function(){
+                        if(!this.get("name")){
+                                this.set({"name": "Anonymous"});
+                        }
+                        if(!this.get("text")){
+                                this.set({"text": "Nothing"});
+                        }
+                        if(!this.get("date")){
+                                this.set({"date": new Date().getTime()});
+                        }
+                }
+        });
+        
+        // Now let's define a new Collection of Comments
+        var CommentCollection = Backbone.Collection.extend({
+                // The couchdb-connector is capable of mapping the url scheme
+                // proposed by the authors of Backbone to documents in your database,
+                // so that you don't have to change existing apps when you switch the sync-strategy
+                url : "/comments",
+                model : CommentModel,
+                // The comments should be ordered by date
+                comparator : function(comment){
+                        return comment.get("date");
+                }
+        });
 
         var DustView = Backbone.View.extend({
             registerTemplate : function(name) {
@@ -59,84 +59,77 @@ $(function(){
                 return this;
             }
         });
-        
-	// Represents an comment entry
-	var EntryView = DustView.extend({
-		tagName : "tr",
-		
-		// Clicking the `X` leads to a deletion
-		events : {
-			"click .delete" : "deleteMe",
-			"dblclick td" : "dummyFetch"
-		},
-		
-		// If there's a change in our model, rerender it
-		initialize : function(){
-			_.bindAll(this, 'render', 'deleteMe', 'dummyFetch');
-			this.model.bind('change', this.render);
+
+        // Represents an comment entry
+        var EntryView = DustView.extend({
+                tagName : "tr",
+                
+                // Clicking the `X` leads to a deletion
+                events : {
+                        "click .delete" : "deleteMe",
+                },
+                
+                // If there's a change in our model, rerender it
+                initialize : function(){
+                        _.bindAll(this, 'render', 'deleteMe', 'dummyFetch');
+                        this.model.bind('change', this.render);
                         this.registerTemplate('entry');
-		},
-		
-		dummyFetch : function(){
-			// Fetch the state of the model from the server.
-			// Used this to test the model sync.
-			this.model.fetch();
-		},
-		
-		// Fade out the element and destroy the model
-		deleteMe : function(){
-			if(this.model)
-				this.model.destroy();
-			$(this.el).fadeOut("fast",function(){
-				$(this).remove();
-			});
-		}
-	});
-	
-	// The view for all comments
-	var CommentsTable = Backbone.View.extend({
-		el: $("#model_table"),
-		
-		initialize : function(){
-			_.bindAll(this, 'refreshed', 'addRow', 'deleted');
-			
-			Comments.bind("refresh", this.refreshed);
-			Comments.bind("add", this.addRow);
-			Comments.bind("remove", this.deleted);
-		},
-		
-		// Prepends an entry row 
-		addRow : function(comment){
-			var view = new EntryView({model: comment});
-			var rendered = view.render().el;
-			this.el.prepend(rendered);
-		},
-		
-		// Renders all comments into the table
-		refreshed : function(){
-			// reset the table
-			$("#model_table").html("");
-			if(Comments.length > 0){
-				// add each element
-				Comments.each(this.addRow);
-			}
-		},
-		
-		// A comment has been deleted, so we rerender the table,
-		// because this update could also come from another user via the
-		// _changes feed
-		deleted : function(){
-			this.refreshed();
-		}
-		
-	});
-	
-	// The App controller initializes the app by calling `Comments.fetch()`
-	var App = Backbone.Controller.extend({
-		initialize : function(){
-			Comments.fetch();
-		}
-	});
+                },
+                
+                // Fade out the element and destroy the model
+                deleteMe : function(){
+                        if(this.model)
+                                this.model.destroy();
+                        $(this.el).fadeOut("fast",function(){
+                                $(this).remove();
+                        });
+                }
+        });
+        
+        // The view for all comments
+        var CommentsTable = Backbone.View.extend({
+                el: $("#model_table"),
+                
+                initialize : function(){
+                        _.bindAll(this, 'refreshed', 'addRow', 'deleted');
+                        
+                        Comments.bind("refresh", this.refreshed);
+                        Comments.bind("add", this.addRow);
+                        Comments.bind("remove", this.deleted);
+                },
+                
+                // Prepends an entry row 
+                addRow : function(comment){
+                        var view = new EntryView({model: comment});
+                        var rendered = view.render().el;
+                        this.el.prepend(rendered);
+                },
+                
+                // Renders all comments into the table
+                refreshed : function(){
+                        // reset the table
+                        $("#model_table").html("");
+                        if(Comments.length > 0){
+                                // add each element
+                                Comments.each(this.addRow);
+                        }
+                },
+                
+                // A comment has been deleted, so we rerender the table,
+                // because this update could also come from another user via the
+                // _changes feed
+                deleted : function(){
+                        this.refreshed();
+                }
+                
+        });
+        
+        // The App controller initializes the app by calling `Comments.fetch()`
+        var App = Backbone.Controller.extend({
+                initialize : function(){
+                        Comments.fetch();
+                }
+        });
 
 
         
@@ -179,14 +172,6 @@ $(function(){
                     values[key] = $(selector).val();
                 }
                 this.collection.create(values);
-                
-//                 var name = $("#model_edit [name='name']").val();
-//                 var text = $("#model_edit [name='text']").val();
-//                 Comments.create({
-//                     "name" : name,
-//                     "text" : text
-//                 });
-//                 return false;
             }
         });
 
@@ -208,7 +193,7 @@ $(function(){
             }
         };
         new SchemaForm({ schema : comment_schema, collection: Comments });
-	new CommentsTable();
-	new App();
+        new CommentsTable();
+        new App();
 
 });

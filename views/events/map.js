447128aad165b67831b0a27250f77e5eef593041
unknown <HAMMDI@.PEROOT.COM>
2011-05-18 13:06:54 -0500

    reverting events view to last night's version

--- views/events/map.js
+++ views/events/map.js
@@ -1,7 +1,7 @@
 function(doc) {
 	// only events, happening today or later
-	if (doc.type=="event" && (new Date( doc.date ) >= new Date() ) ) 
+	if (doc.type=="event") 
 	{
-		emit(doc._id, doc);
+		emit(doc, doc);
 	}
 };

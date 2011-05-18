f059124e23811b049c17a9e0eb6b326d362421fb
unknown <HAMMDI@.PEROOT.COM>
2011-05-17 23:46:06 -0500

    grr... try the old again, with events view

--- _attachments/js/app.js
+++ _attachments/js/app.js
@@ -8,69 +8,35 @@ $(function(){
     // and will provide your models with real time remote updates.
     Backbone.couchConnector.enableChanges = false;
 
-/////////////////////////////////////////////////////////////////////////////
-/// MODEL DECLARATION ///////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////
-	// Band model
-	var BandModel = Backbone.Model.extend({
-		initialize : function() {
-			if (!this.get("name")) this.set({"name": "Generic Band"});
-			if (!this.get("picURL")) this.set({"picURL": "http://images.woome.com/sitemedia/img/picGenericProfile.png"});
-			if (!this.get("bio")) this.set({"bio": "They play musical instruments."});
-		}
-	});
-
-	// Vanue model
-	var VenueModel = Backbone.Model.extend({
-		initialize : function() {
-			if (!this.get("name")) this.set({"name": "Generic Hall"});
-			if (!this.get("picURL")) this.set({"picURL": "http://malhotrarealestate.com/assets/images/generic_house_photo03.jpg"});
-			if (!this.get("bio")) this.set({"bio": "Has four walls and a roof."});
-		}
-	});
-
-    // Event model
-    var EventModel = Backbone.Model.extend({
+    // The model for a comment is kinda simple.
+    // We only need a name, a text and a date.
+    var CommentModel = Backbone.Model.extend({
         initialize : function(){
-            if(!this.get("name")) this.set({"name": "Some generic event"});
-            if(!this.get("description")) this.set({"description": "Go here for fun!"});
-            if(!this.get("hall")) this.set({"hall": "Generic Hall"});
-            if(!this.get("hallPic")) this.set({"hallPic": "http://malhotrarealestate.com/assets/images/generic_house_photo03.jpg"});
-            if(!this.get("band")) this.set({"band": "Generic Band"});
-            if(!this.get("bandPic")) this.set({"bandPic": "http://images.woome.com/sitemedia/img/picGenericProfile.png"});
-			if(!this.get("date")) this.set({"date": new Date().getTime()});
-        },
-		
-/*		toJSON : function() {
-			//TODO: override to JSON to extract band and hall info
-			if ( this.get("hall") ) 
-			{
-				
-				this.set({"hall":
-			}
-		}*/
+            if(!this.get("name")){
+                    this.set({"band": "Anonymous"});
+            }
+            if(!this.get("text")){
+                    this.set({"hall": "Nothing"});
+            }
+            if(!this.get("date")){
+                    this.set({"date": new Date().getTime()});
+            }
+        }
     });
-	
-/////////////////////////////////////////////////////////////////////////////
-/// COLLECTIONS DECLARATION /////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////
-    // Now let's define a new Collection of Events
-    var EventCollection = Backbone.Collection.extend({
+    
+    // Now let's define a new Collection of Comments
+    var CommentCollection = Backbone.Collection.extend({
         // The couchdb-connector is capable of mapping the url scheme
         // proposed by the authors of Backbone to documents in your database,
         // so that you don't have to change existing apps when you switch the sync-strategy
         url : "/events",
-        model : EventModel,
-        // The events should be ordered by date
-        comparator : function(event){
-            return event.get("date");
+        model : CommentModel,
+        // The comments should be ordered by date
+        comparator : function(comment){
+            return comment.get("date");
         }
     });
 
-/////////////////////////////////////////////////////////////////////////////
-/// VIEWS DECLARATION ///////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////
-	// This is the base class for any View using a dust template
     var DustView = Backbone.View.extend({
         registerTemplate : function(name) {
             // Relies on inline templates on the page
@@ -93,29 +59,121 @@ $(function(){
         }
     });
 
-    // Represents an event entry in an event listing; is a dust template
-    var EventEntryView = DustView.extend({
-		
-        // Clicking the feet adds it to the dance card
+    // Represents an comment entry
+    var EntryView = DustView.extend({
+        tagName : "tr",
+        
+        // Clicking the `X` leads to a deletion
         events : {
-            "click .feet" : "addToDanceCard",
+            "click .delete" : "deleteMe",
         },
         
         // If there's a change in our model, rerender it
         initialize : function(){
-            _.bindAll(this, 'render', "addToDanceCard");
+            _.bindAll(this, 'render', 'deleteMe', 'dummyFetch');
             this.model.bind('change', this.render);
-            this.registerTemplate('eventEntry'); // is the "this" valid for this func?  or does it need to be included in the _bindAll ?
+            this.registerTemplate('entry');
         },
         
-        // Adds this event to the danceCard collection
-        addToDanceCard : function(){
-			// nothing here, yet
+        // Fade out the element and destroy the model
+        deleteMe : function(){
+            if(this.model)
+                this.model.destroy();
+            $(this.el).fadeOut("fast",function(){
+                $(this).remove();
+            });
         }
-    });	
+    });
+    
+    // The view for all comments
+    var CommentsTable = Backbone.View.extend({
+        el: $("#model_table"),
+        
+        initialize : function(){
+            _.bindAll(this, 'refreshed', 'addRow', 'deleted');
+            
+            Comments.bind("refresh", this.refreshed);
+            Comments.bind("add", this.addRow);
+            Comments.bind("remove", this.deleted);
+        },
+        
+        // Prepends an entry row 
+        addRow : function(comment){
+            var view = new EntryView({model: comment});
+            var rendered = view.render().el;
+            this.el.prepend(rendered);
+        },
+        
+        // Renders all comments into the table
+        refreshed : function(){
+            // reset the table
+            this.el.html("");
+            // add each element
+            if(Comments.length > 0){
+                Comments.each(this.addRow);
+            }
+        },
+        
+        // A comment has been deleted, so we rerender the table,
+        // because this update could also come from another user via the
+        // _changes feed
+        deleted : function(){
+            this.refreshed();
+        }
+    });
+    
+    // The App controller initializes the app by calling `Comments.fetch()`
+    var App = Backbone.Controller.extend({
+        initialize : function(){
+            Comments.fetch();
+        }
+    });
 
-	// The view for the primary event list container
-    var EventListView = Backbone.View.extend({
+
+    
+    
+    var SchemaForm = Backbone.View.extend({
+        builder: new inputEx.JsonSchema.Builder(),
+
+        el : $("#model_edit"),
+
+        initialize : function(){
+            _.bindAll(this, "onSubmit");
+            this.render();
+        },
+        
+        render : function(){
+            var fields = this.builder.schemaToInputEx(this.options.schema);
+            fields.parentEl = 'model_edit';
+            inputEx(fields);
+            // YUI onClick used rather than Backbone delegateEvents, because
+            //      it started working first. There's probably something simple
+            //      I'm missing, but I don't really care at the moment.
+            new inputEx.widget.Button({
+                id: 'send',
+                parentEl: 'model_edit',
+                type: 'submit',
+                onClick: this.onSubmit,
+                value: 'Send'
+            });
+            return this;
+        },
+
+        // Simply takes the vals from the input fields and 
+        // creates a new Comment.
+        onSubmit : function(){
+            var key, values = {}, selector;
+
+            for (key in this.options.schema.properties)
+            {
+                selector = "#model_edit [name='" + key + "']";
+                values[key] = $(selector).val();
+            }
+            this.collection.create(values);
+        }
+    });
+
+    var SchemaTable = Backbone.View.extend({
         el: $("#model_table"),
 
         initialize : function(){
@@ -127,6 +185,22 @@ $(function(){
         },
 
         render: function(){
+            var header, 
+                cells = [], 
+                fields = this.options.schema.properties;
+
+            this.el.html("");
+
+            for (key in fields)
+            {
+                cells.push( this.make('th',{},fields[key].description) );
+            }
+            cells.push( this.make('th',{},"Edit") );
+            cells.push( this.make('th',{},"Delete") );
+
+            header = this.make('tr',{},cells);
+            this.el.append(header);
+
             if(this.collection.length > 0){
                 this.collection.each(this.addRow);
             }
@@ -134,29 +208,84 @@ $(function(){
         
         // Appends an entry row 
         addRow : function(model){
-            var view = new EventEntryView( { model: model } );
-            this.el.append( view.render().el );
+            var view = new SchemaTableRow({model: model, schema: this.options.schema});
+            var rendered = view.render().el;
+            this.el.append(rendered);
         }
     });
     
-    // The App controller initializes the app by calling `Comments.fetch()`
-    var AppController = Backbone.Controller.extend({
-        initialize : function(){
-            Events.fetch();
+    var SchemaTableRow = Backbone.View.extend({
+        tagName : "tr",
+        
+        // Clicking the `X` leads to a deletion
+        events : {
+            "click .delete" : "deleteMe",
+        },
+
+        render: function(){
+            var row, 
+                fields = this.options.schema.properties;
+
+            for (key in fields)
+            {
+                this.el.appendChild( this.make( 'td', {}, this.model.get(key) ) );
+            }
+            this.el.appendChild( this.make( 'td', {className: 'edit'}, "?" ) );
+            this.el.appendChild( this.make( 'td', {className: 'delete'}, "X" ) );
+            return this;
+        },
+        
+        // Fade out the element and destroy the model
+        deleteMe : function(){
+            if(this.model)
+                this.model.destroy();
+            $(this.el).fadeOut("fast",function(){
+                $(this).remove();
+            });
         }
     });
 
-/////////////////////////////////////////////////////////////////////////////
-/// INSTACIATION & EXECUTION ////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////
-	// create our collection of event models
-	var Events = new EventCollection();
-	
-	// when this inits, it should call Events.fetch(), which should in theory fetch all
-	//	of its data; each model is updated and then triggers a change event which is bound to 
-	//	the EventView.render call.  
-	// When all data is replaced in the collection, the refresh event is triggered which 
-	//	then kicks off the collection's render.
-	// FIXME: this implies, then, that each Model is rendered twice...!?
-	var App = new AppController();
+
+
+
+    var Comments = new CommentCollection();
+/*    comment_schema = {
+        "description":"A comment",
+        "type":"object",
+        "properties":{
+            "name":{
+                "description":"Name",
+                "type":"string"
+            },
+            "text":{
+                "description":"Comment",
+                "placeholder": "Your text",
+                "type":"text"
+            }
+        }
+    };*/
+    comment_schema = {
+        "description":"An event",
+        "type":"object",
+        "properties":{
+            "band":{
+                "description":"Band",
+                "type":"string"
+            },
+            "date":{
+                "description":"Date",
+                "type":"date"
+            },
+			"hall":{
+				"description":"Hall",
+				"type":"string"
+			}
+        }
+    };
+    schemaForm = new SchemaForm({ schema : comment_schema, collection: Comments });
+//     new CommentsTable();
+    schemaTable = new SchemaTable({ schema : comment_schema, collection: Comments });
+    new App();
+//     schemaTable.render();
+
 });

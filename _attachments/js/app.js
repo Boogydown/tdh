20b1699272ce7d264cc34ac8a2ed9ce2aac78727
unknown <HAMMDI@.PEROOT.COM>
2011-05-23 13:17:22 -0500

    added byType view

--- _attachments/js/app.js
+++ _attachments/js/app.js
@@ -3,7 +3,7 @@ $(function(){
     // `ddocName` is the name of your couchapp project.
     Backbone.couchConnector.databaseName = "tdh";
     Backbone.couchConnector.ddocName = "tdh_public";
-    Backbone.couchConnector.viewName = "byCollection";
+    Backbone.couchConnector.viewName = "byType";
     // If set to true, the connector will listen to the changes feed
     // and will provide your models with real time remote updates.
     Backbone.couchConnector.enableChanges = false;

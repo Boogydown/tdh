c486af7551c416ed40365386385aec46319fabd5
unknown <HAMMDI@.PEROOT.COM>
2011-05-28 15:03:39 -0500

    remove datatype and post from google image ajax

--- _attachments/js/app.js
+++ _attachments/js/app.js
@@ -37,8 +37,6 @@ $(function(){
 		
 		getGoogleImage : function () {
 			$.ajax( "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=" + encodeURI( this.get("bandName") ), {
-				dataType: "json",
-				type: "POST",
 				succes: this.setFromGoogleImage
 			});
 		},

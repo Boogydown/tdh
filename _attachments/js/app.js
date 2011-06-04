e183515919c663aacb484df03dab4e05f9885bfa
unknown <HAMMDI@.PEROOT.COM>
2011-06-03 20:02:30 -0500

    this.el

--- _attachments/js/app.js
+++ _attachments/js/app.js
@@ -304,7 +304,7 @@ $(function(){
 			  center: latlng,
 			  mapTypeId: google.maps.MapTypeId.ROADMAP
 			};
-			var map = new google.maps.Map(el.get(), myOptions);
+			var map = new google.maps.Map(this.el.get(), myOptions);
         },
 		
 	});

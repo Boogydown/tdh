e0073b75b47978fda3b414404d657c0819228f3c
Dimitri <Dimitri@VyncUp.com>
2011-06-26 16:06:45 -0500

    get rid of popwith

--- _attachments/js/bbViews.js
+++ _attachments/js/bbViews.js
@@ -70,7 +70,7 @@ VU.PopupView = VU.DustView.extend({
 		this.render();
 		
 		//Fade Popup in and add close button
-		this.el.fadeIn().css({ 'width': Number( popWidth ) }).prepend('<a href="#" class="close"><img src="images/button-x.png" width="21" border="0" class="close_popup" title="Close Window" alt="Close" /></a>');
+		this.el.fadeIn().prepend('<a href="#" class="close"><img src="images/button-x.png" width="21" border="0" class="close_popup" title="Close Window" alt="Close" /></a>');
 
 		//Margin defines center alignment (vertical and horizontal) - add 80px to the height/width for the padding  and border width as defined in the css
 		var popMargTop = (this.el.height() + 80) / 2;

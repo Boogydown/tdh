2ccb36926404c55ec043f3790b3f2c87caef1ba1
Dimitri <Dimitri@VyncUp.com>
2011-06-26 16:17:16 -0500

    tweak popup templates

--- _attachments/js/bbViews.js
+++ _attachments/js/bbViews.js
@@ -69,8 +69,12 @@ VU.PopupView = VU.DustView.extend({
 		this.model = model;
 		this.render();
 		
+		//Fade Background
+		$('body').append('<div id="fade"></div>'); //Add fade layer at the end of the selected tag. 
+		$('#fade').css({'filter' : 'alpha(opacity=80)'}).fadeIn(); //Fade in the fade layer - .css({'filter' : 'alpha(opacity=80)'}) is used to fix the IE Bug on fading transparencies 
+
 		//Fade Popup in and add close button
-		this.el.fadeIn().prepend('<a href="#" class="close"><img src="images/button-x.png" width="21" border="0" class="close_popup" title="Close Window" alt="Close" /></a>');
+		this.el.fadeIn().prepend('<a href="" class="close"><img src="images/button-x.png" width="21" border="0" class="close_popup" title="Close Window" alt="Close" /></a>');
 
 		//Margin defines center alignment (vertical and horizontal) - add 80px to the height/width for the padding  and border width as defined in the css
 		var popMargTop = (this.el.height() + 80) / 2;
@@ -81,10 +85,6 @@ VU.PopupView = VU.DustView.extend({
 			'margin-top' : -popMargTop,
 			'margin-left' : -popMargLeft
 		});
-
-		//Fade Background
-		$('body').append('<div id="fade"></div>'); //Add fade layer at the end of the selected tag. 
-		$('#fade').css({'filter' : 'alpha(opacity=80)'}).fadeIn(); //Fade in the fade layer - .css({'filter' : 'alpha(opacity=80)'}) is used to fix the IE Bug on fading transparencies 
 
 		return false;
 	}

9a3eadebde8dc0348341f0762b3837b117d93c18
Dimitri <Dimitri@VyncUp.com>
2011-07-12 18:56:34 -0500

    sF: wow...loading bar WAY too dark;  fixed field #

--- _attachments/js/sf-app.js
+++ _attachments/js/sf-app.js
@@ -33,7 +33,7 @@ $(function(){
 				this.collsToFetch = 2;
 				if ( !colls.bands.fetched ) { colls.bands.bind( "refresh", this.fetched ); colls.bands.fetch({field:0}) }
 				else this.fetched( colls.bands, {field:0});
-				if ( !colls.halls.fetched ) { colls.halls.bind( "refresh", this.fetched ); colls.halls.fetch({field:4}) }
+				if ( !colls.halls.fetched ) { colls.halls.bind( "refresh", this.fetched ); colls.halls.fetch({field:5}) }
 				else this.fetched( colls.halls, {field:5});
 			}
 			else 

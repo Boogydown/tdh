1d9e6c11f3860b7c1e6b0d72c3a8ac40d1820f19
Dimitri <Dimitri@VyncUp.com>
2011-07-12 12:06:05 -0500

    sF: catch bad setAttr for doc view; image url fix

--- _attachments/js/sf-app.js
+++ _attachments/js/sf-app.js
@@ -178,10 +178,12 @@ $(function(){
 
 		// If there's a change in our model, rerender it
 		initialize : function(){
-			this.el.setAttribute("onclick", "location.href='#doc/"
-				+ this.options.collName + "/" 
-				+ this.options.schemaName + "/" 
-				+ this.model.id + "'");
+			try {
+				this.el.setAttribute("onclick", "location.href='#doc/"
+					+ this.options.collName + "/" 
+					+ this.options.schemaName + "/" 
+					+ this.model.id + "'");
+			} catch (e) {}
 			_.bindAll(this, 'render', "editMe", "deleteMe");
 			this.model.bind('change', this.render);
 			this.registerTemplate( this.options.templateName );
@@ -213,7 +215,7 @@ $(function(){
 						// image?
 						try {
 							if ( row.value.substr(row.value.length - 3 ).toLowerCase() == "jpg")
-								row.value = '<img src="' + this.model.id + "/thumbs/" + row.value + '"/>';
+								row.value = '<img src="../../' + this.model.id + "/thumbs/" + row.value + '"/>';
 						} catch (e) {}
 						
 						// fixed width on long entries

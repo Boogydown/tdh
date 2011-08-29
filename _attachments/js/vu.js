f3b12ab092833ee22707cad3fac95e1498ae1b64
Dimitri <Dimitri@VyncUp.com>
2011-08-29 01:58:51 -0500

    IE7 doesn't have console... duh

--- _attachments/js/vu.js
+++ _attachments/js/vu.js
@@ -3,13 +3,7 @@
 	
 	VU.init = function () {
 		for ( var method in VU )
-		{
-			console.log( "method = " + method );
 			if ( method instanceof Function && method != "init" )
-			{
 				VU[method].call( window );
-				console.log( "call: " + method );
-			}
-		}
 	}
 }).call(window);

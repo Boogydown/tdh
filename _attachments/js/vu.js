d8b51cecbf5e3a11cb72760f3965efc600bcc269
Dimitri <Dimitri@VyncUp.com>
2011-08-29 01:50:01 -0500

    s

--- _attachments/js/vu.js
+++ _attachments/js/vu.js
@@ -3,7 +3,13 @@
 	
 	VU.init = function () {
 		for ( var method in VU )
+		{
+			console.log( "method = " + method );
 			if ( method instanceof Function && method != "init" )
+			{
 				VU[method].call( window );
+				console.log( "call: " + method );
+			}
+		}
 	}
 }).call(window);

49210fd47e527f3c93c19bcf5b541d82c048050d
Dimitri <Dimitri@VyncUp.com>
2011-09-05 10:31:11 -0500

    remove dancecard default, for now

--- _attachments/js/app.js
+++ _attachments/js/app.js
@@ -14,7 +14,7 @@ $(function(){
 	
 	var TDHSessionModel = VU.AuthSessionModel.extend({
 		defaults : {
-			danceCard: new VU.EventsCollection()
+			//danceCard: new VU.EventsCollection()
 		},
 		
 		addToCard : function( eventModel ) {

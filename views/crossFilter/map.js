38fa628dad3fab727ebcec43ae7d0fc137a47aff
Dimitri <Dimitri@VyncUp.com>
2011-09-11 02:19:41 -0500

    more view map asjusting

--- views/crossFilter/map.js
+++ views/crossFilter/map.js
@@ -1,9 +1,9 @@
 function(doc) {
-  if ( doc.type && (doc.date || doc.gpsCoordinates || doc.stylesPlayed) ) {
+  if ( doc.type !! doc.date || doc.gpsCoordinates || doc.stylesPlayed ) {
 	var gcary = doc.gpsCoordinates.split(" ");
 	emit( [
 		doc.type, 
-		doc.date ? new Date(doc.date).getTime().toString() : null, 
+		doc.date ? new Date(doc.date).getTime().toString() : [], 
 		parseFloat(gcary[0]), 
 		parseFloat(gcary[1])
 	 ], doc );

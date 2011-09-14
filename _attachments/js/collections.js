75e113f3b24dbda722fe75572a567f886a6f5de2
Dimitri <Dimitri@VyncUp.com>
2011-09-13 19:58:45 -0500

    re-fetching the events was orphaning the dCard event, so we'll just put a condition on the dcard to only fetch if it's on that tab ... a hack, but will work for now

--- _attachments/js/collections.js
+++ _attachments/js/collections.js
@@ -96,12 +96,19 @@ VU.EventCollection = VU.FilteredCollection.extend({
 	},
 	
 	fetchAndSet : function( idAry, attr ) {
-		this.query = "?startkey=[\"event\"," + new Date().getTime() + "]&endkey=[\"event\",[]]";
+		//HACK: Once cached Colls and View colls get separated, remove this...
 		this._fetchSet = {attrKeys:idAry, attr:attr};
-		this.fetch( {success:this._setAfterFetch} );
+		if ( TDH_tab != "DanceCard" ) {
+			var oldQ = this.query;
+			this.query = "?startkey=[\"event\"," + new Date().getTime() + "]&endkey=[\"event\",[]]";
+			this.fetch( {success:this._setAfterFetch} );
+			this.query = oldQ;
+		} else
+			this.bind( "refresh", this._setAfterFetch );
 	},
 	
 	_setAfterFetch : function(  ){
+		this.unbind( "refresh", this._setAfterFetch );
 		var fetchSet = this._fetchSet,
 			coll = this;
 		_.each( fetchSet.attrKeys, function(modelID) { 

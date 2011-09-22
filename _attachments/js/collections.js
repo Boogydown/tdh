066bad9810284018672163fcc339214b2705741b
Dimitri <Dimitri@VyncUp.com>
2011-09-22 02:16:15 -0500

    tweak band viewName 2

--- _attachments/js/collections.js
+++ _attachments/js/collections.js
@@ -361,7 +361,7 @@ VU.DCardCollection = VU.EventCollection.extend({
 VU.BandCollection = VU.KeyedCollection.extend({
 	url : "band",
 	viewName : "crossFilter",
-	query : '?startkey=["band"]&endkey=["band"]',
+	query : '?startkey=["band",null,null,null]&endkey=["band",[],[],[]]',
 	model : VU.BandModel,
 	comparator : function(band){
 		return band.get("bandName");

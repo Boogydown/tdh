38c25f5f5a11c47a79e93b4b77302c5e531369f3
Dimitri <Dimitri@VyncUp.com>
2011-09-25 09:20:01 -0500

    fix crossfilter view to default to 0

--- views/crossFilter/map.js
+++ views/crossFilter/map.js
@@ -1,10 +1,9 @@
 function(doc) {
   if ( doc.type ) {
-	// since "a" comes after any number we use it include all values if it doesn't exist
-	var gcary = doc.gpsCoordinates ? doc.gpsCoordinates.split(" ") : ["a","a"];
+	var gcary = doc.gpsCoordinates ? doc.gpsCoordinates.split(" ") : [0,0];
 	emit( [
 		doc.type, 
-		doc.date ? new Date(doc.date).getTime() : "a", 
+		doc.date ? new Date(doc.date).getTime() : 0,
 		parseFloat(gcary[0]), 
 		parseFloat(gcary[1])
 	 ], doc );

2a821cf399c759a8c9f4e1d94b56915822fb858b
Dimitri <Dimitri@VyncUp.com>
2011-10-19 23:05:51 -0500

    try adding waiting spinner to scroll of empty boxes

--- _attachments/js/bbViews.js
+++ _attachments/js/bbViews.js
@@ -217,9 +217,12 @@ VU.EventListingView = VU.ListingView.extend({
 	},
 	
 	scrollUpdate : function () {
-		//TODO: interpret scroll
-		if ( this.el.scrollTop >= (this.el.scrollHeight - this.el.clientHeight - this.scrollLoadThreshold ) )
-			this._nextPage();
+		//if scrolling to the bottom then load some more stuff
+		//if ( this.el.scrollTop >= (this.el.scrollHeight - this.el.clientHeight - this.scrollLoadThreshold ) )
+			//this._nextPage();
+		var sTop = this.spacer.position.top();
+		if ( sTop > 0 && sTop < $(this.el).height() )
+			utils.waitingUI.show();
 	},
 	
 	_nextPage : function() {

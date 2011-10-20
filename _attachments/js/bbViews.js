74826d83cd1da79e250a866541d1e78b668334a5
Dimitri <Dimitri@VyncUp.com>
2011-10-19 21:19:06 -0500

    fix spacer el, take 2

--- _attachments/js/bbViews.js
+++ _attachments/js/bbViews.js
@@ -111,9 +111,8 @@ VU.EventListingView = VU.ListingView.extend({
 		this.listingHeight = options.listingHeight || this.LISTING_HEIGHT;
 		this.collection.bind("add", this.addRow);
 		this.collection.bind("remove", this.removeRow);
-		this.spacer = this.make("div", {id:'spacer', style:'height:1px'}, "");
-		$(this.el).append( this.spacer );
-		this.spacer = $(this.spacer);
+		$(this.el).append("<div id='spacer' style='height:1px'></div>");
+		this.spacer = $("#spacer",this.el);
 	},
 	
 	// This will apply filters to the coll and trigger add/remove events, 

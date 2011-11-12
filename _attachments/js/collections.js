602473993fd429d2d714e6f790c419532c78026d
Dimitri Hammond <dimitri@vyncup.com>
2011-11-12 15:54:28 -0600

    sF: #177

--- _attachments/js/collections.js
+++ _attachments/js/collections.js
@@ -174,7 +174,8 @@ VU.LocalFilteredCollection = VU.Collection.extend({
 
 VU.KeyedCollection = VU.Collection.extend({
 	initialize : function(models, options) {
-		options.name && (this.name = _.uniqueId(options.name));
+		if ( options.name !== undefined )
+			this.name = _.uniqueId(options.name);
 		
 		// hash for quickly cross-referencing key matches
 		this.keys = {};

78bbef49bb665a76af405ea90ea4bd098ad778ca
tim <tim.b.mccarter@gmail.com>
2011-11-18 00:41:33 -0600

    fixing modal arrow errors in IE

--- _attachments/js/popupViews.js
+++ _attachments/js/popupViews.js
@@ -16,7 +16,40 @@ Please do not post events that are not in Texas, that are not in a dance hall (e
 Texas Dance Hall Preservation, Inc. reserves the right to reject any submission.\n\n\
 Please click OK if you agree to these terms.')) location.href="#///!";
 	}
-});	
+});
+
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (searchElement /*, fromIndex */) {
+        "use strict";
+        if (this === void 0 || this === null) {
+            throw new TypeError();
+        }
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (len === 0) {
+            return -1;
+        }
+        var n = 0;
+        if (arguments.length > 0) {
+            n = Number(arguments[1]);
+            if (n !== n) { // shortcut for verifying if it's NaN  
+                n = 0;
+            } else if (n !== 0 && n !== Infinity && n !== -Infinity) {
+                n = (n > 0 || -1) * Math.floor(Math.abs(n));
+            }
+        }
+        if (n >= len) {
+            return -1;
+        }
+        var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
+        for (; k < len; k++) {
+            if (k in t && t[k] === searchElement) {
+                return k;
+            }
+        }
+        return -1;
+    }
+}  
 
 //== Events Container Base Class ====================================================================
 VU.EventsContainerPopupView = VU.PopupView.extend({

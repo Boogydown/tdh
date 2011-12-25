8f377499cc0a835b0a77e8429bff28277cefafaf
Dimitri Hammond <dimitri@vyncup.com>
2011-12-24 18:15:29 -0600

    WTF??

--- views/attachments/reduce.js
+++
@@ -1,9 +0,0 @@
-function (keys, values, rereduce){
-if ( !rereduce){
-  var att = [], x, y, len = keys.length;
-    for ( x = 0; x < len; x++ ) 
-      for ( y in values[x] )
-        if ( y != "" )
-	  att.push( keys[x] + "/" + y );
-return att;
-}

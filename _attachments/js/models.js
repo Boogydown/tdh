5632d8aec8f29ed23ed4c33e093f176815f59a19
Dimitri Hammond <dimitri@vyncup.com>
2012-01-31 12:49:11 -0600

    changing default pic

--- _attachments/js/models.js
+++ _attachments/js/models.js
@@ -406,7 +406,7 @@ VU.BandModel = VU.EventsContainerModel.extend({
 	myType : "band",
 	defaults : {
 		bandName: "Generic Band",
-		image: "images/genericSilhouette.jpg",
+		image: "http://dev.vyncup.t9productions.com:44384/tdh/_design/tdh_public/images/genericBand.jpg",
 		bio: "They play musical instruments.",
 		ownerUsers: [],
 		stylesPlayed: [],
